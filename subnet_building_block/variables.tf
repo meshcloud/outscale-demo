437dc98e0b1364c4dd35f79fe97b4e529cf99550
Patrick <pmoore@meshcloud.io>
2025-09-10 09:41:41 +0200

    add required authentication inputs

--- subnet_building_block/variables.tf
+++ subnet_building_block/variables.tf
@@ -1,3 +1,16 @@
+variable "TF_VAR_OUTSCALE_REGION" {
+  type = string
+  nullable = false
+}
+variable "TF_VAR_OUTSCALE_ACCESSKEYID" {
+  type = string
+  nullable = false
+}
+variable "TF_VAR_OUTSCALE_SECRETKEYID" {
+  type = string
+  nullable = false
+  sensitive = true
+}
 variable "net_id" {
   type = string
   description = "ID of the Outscale Net where the Subnet will be created"
