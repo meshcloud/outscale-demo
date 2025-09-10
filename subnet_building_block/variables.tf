72a7da876d5e1820c8bddf3fab6fd93396f19adc
Patrick <pmoore@meshcloud.io>
2025-09-10 10:44:15 +0200

    removed authentication envars

--- subnet_building_block/variables.tf
+++ subnet_building_block/variables.tf
@@ -1,20 +1,3 @@
-variable "OUTSCALE_REGION" {
-  type = string
-  description = "Environment variable required for authentication"
-  nullable = false
-  default = "eu-west-2"
-}
-variable "OUTSCALE_ACCESSKEYID" {
-  type = string
-  description = "Environment variable required for authentication"
-  nullable = false
-}
-variable "OUTSCALE_SECRETKEYID" {
-  type = string
-  description = "Environment variable required for authentication"
-  nullable = false
-  sensitive = true
-}
 variable "net_id" {
   type = string
   description = "ID of the Outscale Net where the Subnet will be created"
@@ -23,3 +6,8 @@ variable "subnet_ip_range" {
   type = string
   description = "IP Range of Subnet in CIDR notation"
 }
+
+
+Access Key ID: 2UIT8KZ26JON554GKDTB
+
+Secret Key: 1FSSMLBDIVFSFMYFCOSV314E3QWNNI02BHBFDXXK
