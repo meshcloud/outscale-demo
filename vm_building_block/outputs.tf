4a59ed506ae37c171af20249a697a76ecf532899
Patrick <pmoore@meshcloud.io>
2025-09-10 15:23:32 +0200

    updated outputs

--- vm_building_block/outputs.tf
+++ vm_building_block/outputs.tf
@@ -4,6 +4,12 @@ output "vm_ip" {
 output "keypair_name" {
   value = outscale_keypair.keypair01.keypair_name
 }
+output "keypair_public_key" {
+  value = outscale_keypair.keypair01.public_key
+}
+output "keypair_private_key" {
+  value = outscale_keypair.keypair01.private_key
+}
 output "private_ip" {
   value = outscale_vm.vm03.private_ip
 }
