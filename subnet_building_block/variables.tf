960b245bd19f584a250fe704a8de0c553575a1c7
Patrick <pmoore@meshcloud.io>
2025-09-10 10:05:32 +0200

    updated vars used for authentication

--- subnet_building_block/variables.tf
+++ subnet_building_block/variables.tf
@@ -1,14 +1,17 @@
-variable "TF_VAR_OUTSCALE_REGION" {
+variable "OUTSCALE_REGION" {
   type = string
+  description = "Environment variable required for authentication"
   nullable = false
   default = "eu-west-2"
 }
-variable "TF_VAR_OUTSCALE_ACCESSKEYID" {
+variable "OUTSCALE_ACCESSKEYID" {
   type = string
+  description = "Environment variable required for authentication"
   nullable = false
 }
-variable "TF_VAR_OUTSCALE_SECRETKEYID" {
+variable "OUTSCALE_SECRETKEYID" {
   type = string
+  description = "Environment variable required for authentication"
   nullable = false
   sensitive = true
 }
