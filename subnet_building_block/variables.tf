e6f681d29e83b8d1d8966d7166bac75d1a8b7fc3
Patrick <pmoore@meshcloud.io>
2025-09-10 09:56:43 +0200

    corrected variable name

--- subnet_building_block/variables.tf
+++ subnet_building_block/variables.tf
@@ -16,7 +16,7 @@ variable "net_id" {
   type = string
   description = "ID of the Outscale Net where the Subnet will be created"
 }
-variable "ip_range" {
+variable "subnet_ip_range" {
   type = string
   description = "IP Range of Subnet in CIDR notation"
 }
