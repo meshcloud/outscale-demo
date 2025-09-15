85e17bebc476a7583c821e018878e2cce776f9f2
Patrick <pmoore@meshcloud.io>
2025-09-15 13:17:08 +0200

    moved security group code to network.tf. testing connection to vm

--- vm_building_block/main.tf
+++ vm_building_block/main.tf
@@ -1,52 +1,28 @@
-resource "outscale_security_group" "security_group01" {
-    description          = "Terraform security group for VM"
-    security_group_name = "terraform-security-group-for-vm"
-    net_id               = var.net_id
-}
-
-resource "outscale_security_group_rule" "security_group_rule01" {
-    flow              = "Inbound"
-    security_group_id = outscale_security_group.security_group01.security_group_id
-    from_port_range   = "8080"
-    to_port_range     = "8080"
-    ip_protocol       = "tcp"
-    ip_range          = var.allow_list_ip_range
-}
-
-resource "outscale_security_group_rule" "security_group_rule02" {
-    flow              = "Inbound"
-    security_group_id = outscale_security_group.security_group01.security_group_id
-    from_port_range   = "22"
-    to_port_range     = "22"
-    ip_protocol       = "tcp"
-    ip_range          = var.allow_list_ip_range
-}
-
 resource "outscale_keypair" "keypair01" {
-    keypair_name = "terraform-keypair-for-vm"
+  keypair_name = "terraform-keypair-for-vm"
 }
 
-resource "outscale_vm" "vm03" {
-    image_id           = var.image_id
-    vm_type            = var.vm_type
-    security_group_ids = [outscale_security_group.security_group01.security_group_id]
-    subnet_id          = var.subnet_id
-    keypair_name = outscale_keypair.keypair01.keypair_name
-    vm_initiated_shutdown_behavior = "terminate"
-    block_device_mappings {
-        device_name = "/dev/sda1" # /dev/sda1 corresponds to the root device of the VM
-        bsu {
-            delete_on_vm_deletion = false
-            volume_size = 15
-            volume_type = "gp2"
-        }
+resource "outscale_vm" "app_vm" {
+  image_id                       = var.image_id
+  vm_type                        = var.vm_type
+  security_group_ids             = [outscale_security_group.app_sg]
+  subnet_id                      = var.subnet_id
+  keypair_name                   = outscale_keypair.keypair01.keypair_name
+  vm_initiated_shutdown_behavior = "terminate"
+  block_device_mappings {
+    device_name = "/dev/sda1" # /dev/sda1 corresponds to the root device of the VM
+    bsu {
+      delete_on_vm_deletion = false
+      volume_size           = 15
+      volume_type           = "gp2"
     }
-    tags {
-        key   = "name"
-        value = "pm-test-vm"
-    }
-    # install docker, pull nodejs helloworld, run in container
-    user_data = base64encode(<<EOF
+  }
+  tags {
+    key   = "name"
+    value = "pm-test-vm"
+  }
+  # install docker, pull nodejs helloworld, run in container
+  user_data = base64encode(<<EOF
 sudo dnf -y install dnf-plugins-core
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -55,7 +31,7 @@ git clone https://github.com/IBM/node-hello-world.git
 docker build -t node-hello-world:latest .
 docker run -it -p 8080:8080 --name node-hello-world node-hello-world:latest
     EOF
-    )
+  )
 }
 
 # resource "outscale_volume" "volume01" {
