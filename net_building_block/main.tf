7f0ef86b52a2be5fc777571537d850323fcf3b5e
Patrick <pmoore@meshcloud.io>
2025-09-22 15:40:48 +0200

    add name tag to net

--- net_building_block/main.tf
+++ net_building_block/main.tf
@@ -1,3 +1,7 @@
 resource "outscale_net" "net02" {
     ip_range = var.net_ip_range
+    tags {
+      key = "name"
+      value = var.net_name
+    }
 }
--- net_building_block/variables.tf
+++ net_building_block/variables.tf
@@ -6,3 +6,7 @@ variable "net_ip_range" {
   type = string
   description = "The network portion of the CIDR block must be between /16 and /28. More info in the docs here https://docs.outscale.com/en/userguide/About-Nets.html"
 }
+variable "net_name" {
+  type = string
+  description = "Name of the new Outscale Net"
+}
